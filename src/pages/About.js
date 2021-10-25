e938b944d00b852b40d33b032d95010aa70b9385
unknown <igor.obrovac990@gmail.com>
2021-10-25 22:18:59 +0200

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, placeat
+        libero nostrum aspernatur, consequatur enim in ipsa eos natus quasi
+        doloribus laboriosam laudantium omnis neque accusamus qui, est iusto
+        fuga quidem. Saepe recusandae ullam, laudantium at aperiam perferendis
+        suscipit tempore?
+      </p>
+    </section>
+  );
+};
+
+export default About;
